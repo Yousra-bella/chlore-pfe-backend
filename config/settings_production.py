b91936aaeb42e5c3d0f7b9e41c601ba32ff460c0
ChubyMonster <bellayoussra42@gmail.com>
2026-07-03 18:37:37 +0100

    Use Railway PostgreSQL

--- config/settings_production.py
+++ config/settings_production.py
@@ -58,11 +58,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("PGDATABASE"),
-        "USER": os.getenv("PGUSER"),
-        "PASSWORD": os.getenv("PGPASSWORD"),
-        "HOST": os.getenv("PGHOST"),
-        "PORT": os.getenv("PGPORT"),
+        "NAME": os.getenv("PGDATABASE", "chlore_db"),
+        "USER": os.getenv("PGUSER", "postgres"),
+        "PASSWORD": os.getenv("PGPASSWORD", "motdepasse"),
+        "HOST": os.getenv("PGHOST", "localhost"),
+        "PORT": os.getenv("PGPORT", "5432"),
     }
 }
  
